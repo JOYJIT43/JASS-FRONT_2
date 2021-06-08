91cf977ad7bb35317099a67471b4627729a1d09c
JOYJIT <joyjitghosh98@gmail.com>
2021-06-09 00:57:32 +0530

    i hope it works

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
-
+  assignment = true;
+  lecture =true;
+  assignment_box = false;
+  lecture_box = false;
   constructor() { }
 
   ngOnInit(): void {
